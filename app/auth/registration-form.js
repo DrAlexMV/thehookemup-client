96c887fa70391e985d786337fc78e10cd1a959dc
DrAlexMV <alexandermventura@utexas.edu>
2014-12-29 02:16:34 -0600

    registration form component

--- app/auth/registration-form.js
+++ app/auth/registration-form.js
@@ -6,40 +6,39 @@ auth.RegistrationForm = function () {
 		lastName: m.prop(''),
 		email: m.prop(''),
 		password: m.prop(''),
-		roles: m.prop([])
+		roles: m.prop([]),
+		displayWarning: m.prop('false')
 	};
 	
 	registrationForm.view = function () {
-
-		var fields = [
-			{ name: 'First Name', parameters: { } }
-		];
+		var vm = registrationForm.vm;
 
 		function formField(name, parameters) {
 			return [
-				m('div.field', [
+				m('div.required.field', [
 					m('label', name),
 					m('input', parameters)
 				])
 			];
 		}
 
+		var fields = [
+			{ name: 'First Name', parameters: { placeholder: 'First Name', onchange: m.withAttr('value', vm.firstName) } },
+			{ name: 'Last Name', parameters: { placeholder: 'Last Name', onchange: m.withAttr('value', vm.lastName) } },
+			{ name: 'Email', parameters: { placeholder: 'Email', onchange: m.withAttr('value', vm.email) } },
+			{ name: 'Password', parameters: { placeholder: 'Password', onchange: m.withAttr('value', vm.password), type: 'password' } }
+		];
+
 		return [
-			m('form.ui.form', [
-				m('div.ui.warning.form', [
+			m('form.ui.form', { classname: vm.displayWarning() ? 'warning' : '' }, [
+				m('div.ui.warning', [
 					m('div.ui.warning.message', [
 						m('div.header', 'Oops!'),
-						m('ul.list', [
-
-						])
+						m('ul.list', [])
 					])
 				]),
-				m('div.two.fields', [
-					m('div.field', [
-					]),
-					m('div.field', [
-
-					])
+				m('div', [
+					fields.map(function (field) { return formField(field.name, field.parameters); })
 				])
 			])
 		]
