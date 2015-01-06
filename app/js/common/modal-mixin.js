477da2abe5b5f56f416a300131bdb5ff31e90308
Austin Stone <austinstone@utexas.edu>
2015-01-05 22:20:31 -0600

    modal mixin

--- app/js/common/modal-mixin.js
+++ app/js/common/modal-mixin.js
@@ -1,149 +1,46 @@
-<!doctype html>
-    <html lang="en">
-        <head>
-            <link href="select2-3.5.2/select2.css" rel="stylesheet"/>
-            <script type="text/javascript" src="jquery-1.11.2.min.js"></script>
-            <script type="text/javascript" src="mithril.js"></script>
-            <script type="text/javascript" src="select2-3.5.2/select2.js"></script>
-            <script type="text/javascript" src="Semantic-UI-1.5.2/dist/semantic.min.js"></script>
-            <link rel="stylesheet" href="Semantic-UI-1.5.2/dist/semantic.min.css">
+var ModalMixin = function (body) {
 
+	var modal = {};
 
-            </head>
-            <body>
+	var vm =
+	modal.vm = {
+		init: function () {
+			vm.show = m.prop(false);
 
-                <div id ="somename">
-                </div>
+			vm.open = function () {
+				vm.show(true);
+			};
 
+			vm.close = function () {
+				vm.show(false)
+			};
 
-                <script type="text/javascript">
+			vm.body = body;
+		}
+	};
 
+	function config(element, isInitialized) {
+		if (vm.show() && !isInitialized) {
+			$(element).modal('show');
+		} else {
+			isInitialized && $(element).modal('hide');
+		}
+	}
 
+	modal.controller = function () {
+		vm.init();
+		body.controller();
+	};
 
-                var ModalMixin = function () {
+	modal.view = function () {
+		return [
+			m('div.ui.basic.modal', { config: config } , [
+				body.view()
+			])
+		];
+	};
 
+	return modal;
+};
 
-
-                    return function (body) {
-                    var modal = {};
-
-
-                    modal.vm = {
-                    body: body,
-                    displayed: m.prop(false)
-                    };
-
-
-
-                    //modal.stream = vm.body.stream;
-
-                    modal.controller = function () {
-                    var vm = modal.vm
-
-                    this.checkDisplayed = function(){
-                    return vm.displayed()
-                    }
-
-                    this.display = function () {
-                    vm.displayed(true)
-                    }
-                    this.hide=function(){
-                    vm.displayed(false)
-                    }
-
-                    };
-
-
-                    modal.config = function(ctrl) {
-                    return function(element, isInitialized) {
-
-                    var el = $(element);
-
-                    if (!isInitialized) {
-                    //set up select2 (only if not initialized already)
-                    document.write(ctrl.checkDisplayed())
-                    if(ctrl.checkDisplayed()==true)
-                    {
-                    $('.modal').modal('show');
-                    }
-
-                    //this event handler updates the controller when the view changes
-                    el.on("change", function(e) {
-                    //integrate with the auto-redrawing system...
-                    m.startComputation();
-
-                    //...so that Mithril autoredraws the view after calling the controller callback
-                    document.write("modal changed");
-
-                    m.endComputation();
-                    //end integration
-                    });
-                    }
-
-                    //update the view with the latest controller value
-                    //document.write("modal changed 2");
-                    }
-                    }
-
-
-                    /*[
-                     m('input[type=text]', { value:"test value",  onchange: m.withAttr(true, ctrl.displayed) }),
-                     m("div", {config: body.config(ctrl)}, [
-                     body.view()
-                     ])
-                     ]*/
-
-                    //this view implements select2's `<select>` progressive enhancement mode
-                    modal.view = function(ctrl) {
-                    return ([
-                    m('button',{onclick: function(e){$('.modal').modal('show');}},"Click me"),
-                    body.view()
-                    ])
-                    };
-
-                    return modal;
-                    };
-                    };
-                body = {}
-                body.view =function(ctrl){ return m('div.ui basic modal',  [
-                    m('div.ui basic modal',[
-                    m('i.close icon',[
-                    m('div.header', 'Archive Old Messages'),
-                    m('div.content',[
-                    m('div.image',[
-                    m('i.archive icon')
-
-                    ]),
-                    m('div.description',[
-                    m('p', 'Your inbox is getting full, would you like us to enable automatic archiving of old messages?')
-                    ])
-
-                    ]),
-                    m('div.actions',[
-                    m('div.two fluid ui inverted buttons',[
-                    m('div.ui red basic inverted button',[
-                    m('i.remove icon','No')
-
-                    ]),
-                    m('div.ui green basic inverted button',[
-                    m('i.checkmark icon','Yes')
-                    ])
-                    ])
-                    ])
-                    ])
-                    ])
-                    ])}
-
-                body.controller=function()
-{
-    this.prop = function () {
-        return 0
-    };
-
-    }
-                //initialize
-                m.module(document.getElementById("somename"), ModalMixin()(body));
-                //module.exports = ModalMixin;
-                </script>
-            </html>
-
+module.exports = ModalMixin;
