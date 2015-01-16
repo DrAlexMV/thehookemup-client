642cc58e0b32a24deaf0ecbba6a81af3811e2233
Austin Stone <austinstone@utexas.edu>
2015-01-15 23:18:02 -0600

    added message to connect modal

--- app/js/profile/connect-with.js
+++ app/js/profile/connect-with.js
@@ -9,11 +9,13 @@ var ConnectWith = function (basicUserInfo) {
   connectWith.stream = new Bacon.Bus();
 
   function connect(whichButton) {
+    console.log(vm.message());
     connectWith.stream.push(new StreamCommon.Message('ConnectWithModal::'+whichButton, {}));
   }
 
   var vm = connectWith.vm = {
-    profilePicture: new EditableImage(false)
+    profilePicture: new EditableImage(false),
+    message: m.prop()
   };
 
   connectWith.controller = function(basicUserInfo) {
@@ -24,14 +26,29 @@ var ConnectWith = function (basicUserInfo) {
 		return [
 			m("i.close.icon"),
       m("div.header","Do you wish to connect to " + basicUserInfo().firstName() + " " + basicUserInfo().lastName() + "?"),
-      m("div.content",[
-        m("div.ui.medium.bordered.image",[
-          connectWith.vm.profilePicture.view({userImageURL: basicUserInfo().picture()})
-        ]),
-        m("div.description",[
-          m("div.ui.header","Expand your network!"),
-          m("p","Choosing to connect will send a notification to this person."),
-          m("p","After you are connected, you will have full access to this person's contact information, including email and phone number.")
+      m("div.ui.padded.grid",[
+       m("div.six.wide.column",[
+        m("div.content",[
+          m("div.ui.medium.bordered.image",[
+            connectWith.vm.profilePicture.view({userImageURL: basicUserInfo().picture()})
+          ])
+          ])
+         ]),
+         m("div.ten.wide.column",[
+          m("div.description",[
+            m("div.ui.header","Expand your network!"),
+            m("p","Choosing to connect will send a notification to this person. After you are connected, you will have full access to this person's contact information, including email and phone number.")
+           // m("p","After you are connected, you will have full access to this person's contact information, including email and phone number.")
+          ]),
+
+
+        m("div.content",[
+          m("form.ui.form",[
+            m("div.field",[
+              m("br"),
+              m("textarea[placeholder=Please enter a message to "+basicUserInfo().firstName()+" about why you want to connect.]",{onchange: m.withAttr('value', vm.message)})
+            ])
+          ])
         ])
       ]),
       m("div.actions",[
@@ -40,7 +57,8 @@ var ConnectWith = function (basicUserInfo) {
           m("i.checkmark.icon")
         ])
       ])
-    ];
+    ])
+      ];
 	};
 
   return connectWith;
