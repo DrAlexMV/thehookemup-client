425cc69c0103bfe09fb175582ea2cbbf1ab299b8
Nicholas Sundin <nsundin2@gmail.com>
2015-02-06 15:41:42 -0600

    Stylistic changes. Made new notications show jewel.

--- app/js/navigation/connect-message.js
+++ app/js/navigation/connect-message.js
@@ -1,72 +1,66 @@
-/**
- * Created by austinstone on 1/18/15.
- */
-
-
 var EditableImage = require('common/editable-image');
 var StreamCommon = require('common/stream-common');
 
 var ConnectMessage = function (basicUserInfo, message) {
-  var connectMessage = {};
+	var connectMessage = {};
 
-  connectMessage.stream = new Bacon.Bus();
+	connectMessage.stream = new Bacon.Bus();
 
-  function connect(whichButton) {
-    connectMessage.stream.push(new StreamCommon.Message('ConnectMessageModal::'+whichButton, {}));
-  }
+	function connect(whichButton) {
+		connectMessage.stream.push(new StreamCommon.Message('ConnectMessageModal::'+whichButton, {}));
+	}
 
-  var vm = connectMessage.vm = {
-    profilePicture: new EditableImage(false),
-    message: message
-  };
+	var vm = connectMessage.vm = {
+		profilePicture: new EditableImage(false),
+		message: message
+	};
 
-  connectMessage.controller = function(basicUserInfo) {
+	connectMessage.controller = function(basicUserInfo) {
 
-  };
+	};
 
-  connectMessage.view = function () {
-    return [
-      m("i.close.icon"),
-      m("div.header","Message from " + basicUserInfo().firstName() + " " + basicUserInfo().lastName() + ":"),
-      m("div.ui.padded.grid",[
-        m("div.six.wide.column",[
-          m("div.content",[
-            m("div.ui.medium.bordered.image",[
-              connectMessage.vm.profilePicture.view({userImageURL: basicUserInfo().picture()})
-            ])
-          ])
-        ]),
-        m("div.ten.wide.column",[
-          m("div.content",[
-            m("div.field",[
-              m("br"),
-              m("div.ui.segment",[
-                m("i.quote.left.icon"),
-                m("content",vm.message),
-                m("i.quote.right.icon")
-              ])
-            ])
-          ]),
-          m('br'),
-          m("div.description",[
-            m("div.ui.header","Expand your network!"),
-            m("p","Choosing to connect will give "+ basicUserInfo().firstName() +
-              " access to your email address and phone number. You will also have full access to " +
-              basicUserInfo().firstName() + "'s contact information, including email and phone number.")
-            // m("p","After you are connected, you will have full access to this person's contact information, including email and phone number.")
-          ])
-        ]),
-        m("div.actions",[
-          m("div.ui.black.button",{onclick: connect.bind(connect,"NoConnect")}, "Not Now"),
-          m("div.ui.positive.right.labeled.icon.button",{onclick: connect.bind(connect,"Connect")},"Connect",[
-            m("i.checkmark.icon")
-          ])
-        ])
-      ])
-    ];
-  };
+	connectMessage.view = function () {
+		return [
+			m("i.close.icon"),
+			m("div.header","Message from " + basicUserInfo().firstName() + " " + basicUserInfo().lastName() + ":"),
+			m("div.ui.padded.grid",[
+				m("div.six.wide.column",[
+					m("div.content",[
+						m("div.ui.medium.bordered.image",[
+							connectMessage.vm.profilePicture.view({userImageURL: basicUserInfo().picture()})
+						])
+					])
+				]),
+				m("div.ten.wide.column",[
+					m("div.content",[
+						m("div.field",[
+							m("br"),
+							m("div.ui.segment",[
+								m("i.quote.left.icon"),
+								m("content",vm.message),
+								m("i.quote.right.icon")
+							])
+						])
+					]),
+					m('br'),
+					m("div.description",[
+						m("div.ui.header","Expand your network!"),
+						m("p","Choosing to connect will give "+ basicUserInfo().firstName() +
+							" access to your email address and phone number. You will also have full access to " +
+							basicUserInfo().firstName() + "'s contact information, including email and phone number.")
+					])
+				]),
+				m("div.actions",[
+					m("div.ui.black.button",{onclick: connect.bind(connect,"NoConnect")}, "Not Now"),
+					m("div.ui.positive.right.labeled.icon.button",{onclick: connect.bind(connect,"Connect")},"Connect",[
+						m("i.checkmark.icon")
+					])
+				])
+			])
+		];
+	};
 
-  return connectMessage;
+	return connectMessage;
 };
 
 module.exports = ConnectMessage;
--- app/js/navigation/notification-list.js
+++ app/js/navigation/notification-list.js
@@ -13,146 +13,142 @@ var UserEdges = require('model/user-edges');
  */
 
 var NotificationList = function (edges) { // edges is an m.prop
-  var notificationList = {};
+	var notificationList = {};
 
-  //currently unused
-  notificationList.stream = m.prop(new Bacon.Bus());
+	//currently unused
+	notificationList.stream = m.prop(new Bacon.Bus());
 
-  function respond(response, userId, userIndex) {
-    /*response is Connect or NoConnect whether the request was accepted*/
-    if (response === 'Connect') {
-      var user = edges().pendingConnections()[userIndex];
+	function respond(response, userId, userIndex) {
+		/*response is Connect or NoConnect whether the request was accepted*/
+		if (response === 'Connect') {
+			var user = edges().pendingConnections()[userIndex];
 
-      UserEdges.connectMe(userId).then(function (response) {
-        var es = edges();
-        var cs = es.connections();
-        cs.push(user);
-        es.connections(cs);
+			UserEdges.connectMe(userId).then(function (response) {
+				var es = edges();
+				var cs = es.connections();
+				cs.push(user);
+				es.connections(cs);
 
-        var pc = es.pendingConnections();
-        pc.splice(userIndex, 1);
-        es.pendingConnections(pc);
+				var pc = es.pendingConnections();
+				pc.splice(userIndex, 1);
+				es.pendingConnections(pc);
 
-        Context.setCurrentUserEdges(es);
+				Context.setCurrentUserEdges(es);
 
-        notificationList.stream().push(
-          new StreamCommon.Message('NotificationList::Connect', { user: user })
-        );
-      });
-    } else if (response === 'NoConnect') {
-      UserEdges.deleteConnection(userId).then(function () {
-        var es = edges();
-        var pc = es.pendingConnections();
-        pc.splice(userIndex, 1);
-        es.pendingConnections(pc);
+				notificationList.stream().push(
+					new StreamCommon.Message('NotificationList::Connect', { user: user })
+				);
+			});
+		} else if (response === 'NoConnect') {
+			UserEdges.deleteConnection(userId).then(function () {
+				var es = edges();
+				var pc = es.pendingConnections();
+				pc.splice(userIndex, 1);
+				es.pendingConnections(pc);
 
-        Context.setCurrentUserEdges(es);
+				Context.setCurrentUserEdges(es);
 
-        notificationList.stream().push(
-          new StreamCommon.Message('NotificationList::NoConnect', { user: user })
-        );
-      });
-    }
-  }
+				notificationList.stream().push(
+					new StreamCommon.Message('NotificationList::NoConnect', { user: user })
+				);
+			});
+		}
+	}
 
-  function truncate(string, n) {
-    return string.length > n ? string.substr(0, n - 1) + '...' : string + '!';
-  }
+	function truncate(string, n) {
+		return string.length > n ? string.substr(0, n - 1) + '...' : string;
+	}
 
-  notificationList.view = function () {
-    var list = [];
+	notificationList.view = function () {
+		var list = null;
 
-    if (edges().pendingConnections()) {
-      list = edges().pendingConnections().map(function (user, idx) {
+		if (edges().pendingConnections()) {
+			list = edges().pendingConnections().map(function (user, idx) {
 
-        var messageView = {};
+				var messageView = {};
 
-        //Did this user send a message?
-        if (edges().pendingConnectionsMessages()[user._id()] && edges().pendingConnectionsMessages()[user._id()] != '') {
-          var message = edges().pendingConnectionsMessages()[user._id()];
-          messageView = [
-            //TODO: figure out how to hyphenate the message. Standard hyphens: auto causes overflow
-            m("div.content", [
-              m("div[style=word-break: break-all; white-space: normal;]", [
-                m("i.quote.left.icon"),
-                truncate(message, 85),
-                m("i.quote.right.icon")
-              ])
-            ]),
-            m("div.ui.divider")
-          ];
-        }
+				//Did this user send a message?
+				if (edges().pendingConnectionsMessages()[user._id()] && edges().pendingConnectionsMessages()[user._id()] != '') {
+					var message = edges().pendingConnectionsMessages()[user._id()];
+					messageView = [
+						//TODO: figure out how to hyphenate the message. Standard hyphens: auto causes overflow
+						m("div.content", [
+							m("div[style=word-break: break-all; white-space: normal;]", [
+								m("i.quote.left.icon"),
+								truncate(message, 85),
+								m("i.quote.right.icon")
+							])
+						]),
+						m("div.ui.divider")
+					];
+				}
 
-        return [
-          m('div.ui.segment', [
-            m('div.ui.card[style=border: 1px solid Gainsboro;]', [
-              m('div.content', [
-                m('div.header', [
-                  m('img.ui.avatar.image', { src: User.getPicture(user) }),
-                  m('a', {href: '/profile/' + user._id(), config: m.route}, [
-                      'Request from ' + truncate(User.getName(user), 14)
-                  ])
-                ]),
-                m('div.ui.center.aligned.segment[style=border: 1px solid Gainsboro;]', [
-                  messageView,
-                  m('div.description', 'Would you like to connect?'),
-                  m("br"),
+				return [
+					m('div.ui.segment', [
+						m('div.ui.card[style=border: 1px solid Gainsboro;]', [
+							m('div.content', [
+								m('div.header', [
+									m('img.ui.avatar.image', { src: User.getPicture(user) }),
+									m('a', {href: '/profile/' + user._id(), config: m.route}, [
+											'Request from ' + truncate(User.getName(user), 14)
+									])
+								]),
+								m('div.ui.center.aligned.segment[style=border: 1px solid Gainsboro;]', [
+									messageView,
+									m('div.description', 'Would you like to connect?'),
+									m("br"),
 
-                  m('div.ui.padded.grid', [
-                    m('div.ui.center.aligned.one.column.row',[
-                    m("div.ui.black.button", {onclick: respond.bind(respond, 'NoConnect', user._id())}, "Not Now"),
-                    m("div.ui.positive.right.labeled.icon.button", {onclick: respond.bind(respond, 'Connect', user._id())}, "Connect", [
-                      m("i.checkmark.icon")])
-                   // m('div.ui.green.button', {onclick: respond.bind(respond, 'Connect', user._id(), idx)}, 'Yes'),
-                      // m('div.ui.red.button', {onclick: respond.bind(respond, 'NoConnect', user._id(), idx)}, 'No')
-                      ])
-                  ])
-                ])
-              ])
-            ])
-          ])
-        ];
-      });
-    }
+									m('div.ui.padded.grid', [
+										m('div.ui.center.aligned.one.column.row',[
+										m("div.ui.black.button", {onclick: respond.bind(respond, 'NoConnect', user._id())}, "Not Now"),
+										m("div.ui.positive.right.labeled.icon.button", {onclick: respond.bind(respond, 'Connect', user._id())}, "Connect", [
+											m("i.checkmark.icon")])
+										])
+									])
+								])
+							])
+						])
+					])
+				];
+			});
+		}
 
-    var numPending = edges().pendingConnections().length;
-    if (numPending === 0) {
-      return [
-        m('i.alarm.outline.icon'),
-        m('div.menu', [
-          m('div.item', [
-            m('div.ui.card', [
-              m('div.content', [
-                m('div.description', 'You don\'t have any notifications right now.')
-              ])
-            ])
-          ])
-        ])
-      ];
-    }
+		var numPending = edges().pendingConnections().length;
+		var newNotifsBubble = null;
+		if (numPending == 0) {
+			list = [
+				m('div.item', [
+					m('div.ui.card', [
+						m('div.content', [
+							m('div.description', 'You don\'t have any new notifications.')
+						])
+					])
+				])
+			];
+		} else {
+			newNotifsBubble = m('div.floating.ui.red.mini.label', numPending);
+		}
 
-    return [
-      m('i.alarm.icon'), numPending,
-      m("div.menu", [
-        m("div", [
-          m("div[style='overflow:auto;max-height:500px;']", [
-            list
-          ])
-        ]),
+		return [
+			m('i.alarm.outline.icon'), newNotifsBubble,
+			m('div.menu', [
+				m('div', [
+					m('div#nav-notification-list', [
+						list
+					])
+				]),
 
-        m("div.item", [
-          m('a', { href: '/notifications/', config: m.route }, [
-            m("div.ui.center.aligned.segment[style=border: 1px solid Gainsboro;]", [
-              m("div.content", "See all")
-            ])
-          ])
-        ])
-      ])
+				m('div.ui.right.aligned.item', [
+					m('a', { href: '/notifications/', config: m.route }, [
+						'See all'
+					])
+				])
+			])
 
-    ]
-  };
+		]
+	};
 
-  return notificationList;
+	return notificationList;
 };
 
 module.exports = NotificationList;
