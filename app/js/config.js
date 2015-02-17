1196e1cca30c97121e1e3b808bcb4fe7ec431991
Nicholas Sundin <nsundin2@gmail.com>
2015-02-17 02:29:17 -0600

    Calculate API url based on current window.location.href:

--- app/js/config.js
+++ app/js/config.js
@@ -1,6 +1,12 @@
 var config = {};
 
-config['API_URL'] = 'http://127.0.0.1:5000';
+var calculateAPIURL = function(currentLocation, port) {
+	var arr = currentLocation.split("/");
+	return arr[0] + '//' + arr[2].split(':')[0] + ':' + port;
+};
+
+config['API_PORT'] = 5000;
+config['API_URL'] = calculateAPIURL(window.location.href, config['API_PORT']);
 config['API_BASE'] = '/api/v1';
 config['FACEBOOK_APP_ID'] = '744792018972866'
 
