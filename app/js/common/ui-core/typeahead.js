cadbadcbab46aa8156fbca4bffd2100d99d79ac2
Austin Stone <austinstone@utexas.edu>
2015-02-22 18:20:09 -0600

    Adding arcconfig.
    Summary:
    Typeahead works great, but I'm in the progress of cleaning up the code.
    
    Typeahead for skills now works
    
    Test Plan: I tested it briefly on localhost. Everything seemed good.
    
    Reviewers: DrAlexMV
    
    Subscribers: nsundin
    
    Differential Revision: http://beta.founderati.io/D2

--- app/js/common/ui-core/typeahead.js
+++ app/js/common/ui-core/typeahead.js
@@ -1,30 +1,78 @@
+/**
+ * @jsx m
+ */
+
 var API = require('common/api');
 
-var Typeahead = (function(entity, state) {
+var Typeahead = function (entity, state, placeholderText, numberResults) {
 
 	var typeahead = {};
 
-	typeahead.getSuggestions = function(params, entity, state) {
+	typeahead.getSuggestions = function (params) {
 		var deferred = m.deferred();
-		console.log("PArams are: " + params);
 		var stringified = jQuery.param(params);
-		console.log("Here in getSuggestions. Stringified is: " + stringified);
-		console.log("Entity is: " + entity);
 		this.get('/search/autocomplete/' + entity + '?' + stringified).then(
-		function(response) {
-			console.log("Here in the callback");
-			state(response['results'][0]['text']);
-			console.log("Results are " + response['results'][0]['text']);
-			deferred.resolve(response['results'][0]['text']);
-		});
+			function (response) {
+				if (response['results'].length == 0) {
+					deferred.resolve([]);
+				} else {
+					deferred.resolve(response['results']);
+				}
+			});
 		return deferred.promise;
+	};
 
-		
+	typeahead.keyAction = function () {
+		var inputValue = document.getElementById("inputValue");
+		var s = inputValue.value;
+		if (s != '') {
+			typeahead.getSuggestions({
+				text: s,
+				results: numberResults
+			}).then(function (results) {
+				//TODO: a way to do this without jquery?
+				suggestHolder = $('.suggest-holder ul');
+				suggestHolder.empty();
+				if (results.length == 0) {
+					suggestHolder.hide();
+				} else {
+					for (var i in results) {
+						suggestHolder.append($("<li><span class='suggest-name'>" + results[i]['text'] +
+							"</span><span class='suggest-description'>" + "Popularity: " +
+							results[i]['score'] + "</span></li>"));
+					}
+					$('.suggest-holder li').on('click', function(){
+						var selectedValue = $(this).find('.suggest-name').html();
+						document.getElementById("inputValue").value=selectedValue;
+						state(selectedValue);
+						suggestHolder.hide();
+					});
+					suggestHolder.show();
+				}
+			})
+		}
+	};
+
+	typeahead.view = function () {
+		return (
+			<div class = "suggest-holder">
+				<input
+				id="inputValue"
+				class="suggest-prompt"
+				type="text"
+				placeholder={placeholderText}
+				onkeypress={typeahead.keyAction}
+				onkeyup={typeahead.keyAction}
+				onchange={m.withAttr("value", state)}
+				/>
+				<ul></ul>
+			</div>
+			)
 	};
 
 	_.mixin(typeahead, API);
 	return typeahead
-})();
+};
 
 module.exports = Typeahead;
 
